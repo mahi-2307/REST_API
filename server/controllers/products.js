9e5e6b898e0681478800c9584621ee447714f959
Mahidhar <mahireddy.katam@gmail.com>
2023-10-10 21:59:05 +0530

    changes

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -2,7 +2,17 @@ const Product = require("../models/product");
 
 const getAllProducts = async (req, res) => {
   const { company, featured, sort, select } = req.query;
+  const { search } = req.query;
+  const { minPrice, maxPrice } = req.query;
   const queryObject = {};
+if (minPrice && maxPrice) {
+  queryObject.price = { $gte: minPrice, $lte: maxPrice };
+}
+
+  if (search) {
+    queryObject.$text = { $search: search };
+  }
+
   if (company) {
     queryObject.company = company;
     console.log(queryObject.company);
@@ -12,6 +22,11 @@ const getAllProducts = async (req, res) => {
     console.log(queryObject.featured);
   }
   let dataFetch = Product.find(queryObject);
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  dataFetch = dataFetch.skip(skip).limit(limit);
 
   if (sort) {
     let sortOut = sort.replace(",", " ");
@@ -23,8 +38,12 @@ const getAllProducts = async (req, res) => {
     dataFetch = dataFetch.select(sortOut);
   }
 
-  const getData = await dataFetch;
-  res.status(200).json({ getData });
-};
-
+  try {
+    const getData = await dataFetch;
+    res.status(200).json({ data: getData, totalProducts });
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+}
+  
 module.exports = { getAllProducts };
